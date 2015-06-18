30afe4c248440119aad9371e16443c132569a035
Peter Li <tyuo9980@gmail.com>
2015-06-17 23:54:19 -0400

    ignore ds store

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Straights
+//
+//  Created by Peter Li on 2015-06-17.
+//  Copyright (c) 2015 Peter Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
